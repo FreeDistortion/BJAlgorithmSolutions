292d17ce5dfb0f3043d6d1c53538b96e0f5df6f7
KT <clouderneering@gmail.com>
2023-09-23 18:50:44 +0900

    092223

--- src/Sep1823/no2588.java
+++ src/Sep1823/no2588.java
@@ -22,14 +22,26 @@
 */
 package Sep1823;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class no2588 {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		long a = sc.nextLong();
-		
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb= new StringBuilder();
+		int a = Integer.parseInt(bf.readLine());
+		char[] b = bf.readLine().toCharArray();
+		int l=b.length;
+		int sum=0;
+		for(int i=0;i<l;i++) {
+			sb.append(a*(b[l-i-1]-'0'));
+			sb.append("\n");
+			sum+=a*(b[l-i-1]-'0')*Math.pow(10, i);
+		}
+		sb.append(sum);
+		System.out.println(sb);
 	}
 
 }
